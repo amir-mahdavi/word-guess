b3f636f2ff7e36c654a095c61bb936ab536dc3e8
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-17 13:49:14 +1000

    check prop types in tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,14 +1,19 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 /**
  * Functional react component for congratulatory message
  * @param {object} props - React props
  * @returns {JSX.Element} - Rendered component (or null if 'success' props is empty)
  */
-export default props => (
+const Congrats = props => (
   <div data-test="component-congrats">
     {props.success ? (
       <span data-test="congrats-message">Congratulations! You guessed the word!</span>
     ) : null}
   </div>
 );
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
+};
+export default Congrats;
