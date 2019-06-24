1059ab7675daa1f4a668d01b75e48857867a72c2
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-24 16:43:19 +1000

    Completed test and dev for success case

--- src/Input.js
+++ src/Input.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Input extends Component {
-  render() {
+  renderContents() {
     return this.props.success ? null : (
-      <div data-test="component-input">
-        <form className="form-inline">
-          <input
-            data-test="input-box"
-            className="mb-2 mx-sm-3"
-            id="word-guess"
-            type="text"
-            placeholder="enter your guess"
-          />
-          <button data-test="submit-button" className="btn btn-primary mb-2" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          id="word-guess"
+          type="text"
+          placeholder="enter your guess"
+        />
+        <button data-test="submit-button" className="btn btn-primary mb-2" type="submit">
+          Submit
+        </button>
+      </form>
     );
+  }
+  render() {
+    return <div data-test="component-input">{this.renderContents()} </div>;
   }
 }
 
